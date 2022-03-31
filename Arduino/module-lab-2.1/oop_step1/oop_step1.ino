f3d7fa1490755b0922c8cee1ebcd25fc6a2cd37f
Alessandro Ricci <a.ricci@unibo.it>
2022-03-31 11:58:40 +0200

    Lab 20220331

--- Arduino/module-lab-2.1/oop_step1/oop_step1.ino
+++ Arduino/module-lab-2.1/oop_step1/oop_step1.ino
@@ -11,14 +11,9 @@ void setup(){
 void loop(){
   int x = 0;
   
-  Counter* c2 = new Counter(20);
-  Serial.println(String("stack at: ") + ((int)(&x)) + " heap at: " + ((int)c2));
-  delay(100);
-  /*  
   c.inc();
   Serial.println(c.getValue());
   c1->inc();
   Serial.println(c1->getValue());
   delay(1000);
-  */
 }
